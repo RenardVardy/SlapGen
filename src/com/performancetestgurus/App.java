4892e1cf29b38105967f68f47f52390f100924e3
Renard Vardy <renard.vardy@performancetestgurus.com>
2019-04-30 22:05:36 +1000

    Added file  dialog for make it easy to add file names

--- src/com/performancetestgurus/App.java
+++ src/com/performancetestgurus/App.java
@@ -10,13 +10,17 @@ import java.awt.BorderLayout;
 import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
 
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+import javax.swing.filechooser.FileNameExtensionFilter;
+import javax.swing.filechooser.FileSystemView;
 
 /**
  * Simple UI for creating slaps (Right hand slap, left hand slap and thumb slaps) 
@@ -43,19 +47,34 @@ import javax.swing.JTextField;
 
 public class App implements ActionListener
 {
-    JTextField fingerTextField01 = new JTextField(100);
-    JTextField fingerTextField02 = new JTextField(100);
-    JTextField fingerTextField03 = new JTextField(100);
-    JTextField fingerTextField04 = new JTextField(100);
-    JTextField fingerTextField05 = new JTextField(100);
-    JTextField fingerTextField06 = new JTextField(100);
-    JTextField fingerTextField07 = new JTextField(100);
-    JTextField fingerTextField08 = new JTextField(100);
-    JTextField fingerTextField09 = new JTextField(100);
-    JTextField fingerTextField10 = new JTextField(100);
-    JTextField fingerTextField13 = new JTextField(100);
-    JTextField fingerTextField14 = new JTextField(100);	    
-    JTextField fingerTextField15 = new JTextField(100);
+    private JTextField fingerTextField01 = new JTextField(100);
+    private JTextField fingerTextField02 = new JTextField(100);
+    private JTextField fingerTextField03 = new JTextField(100);
+    private JTextField fingerTextField04 = new JTextField(100);
+    private JTextField fingerTextField05 = new JTextField(100);
+    private JTextField fingerTextField06 = new JTextField(100);
+    private JTextField fingerTextField07 = new JTextField(100);
+    private JTextField fingerTextField08 = new JTextField(100);
+    private JTextField fingerTextField09 = new JTextField(100);
+    private JTextField fingerTextField10 = new JTextField(100);
+    private JTextField fingerTextField13 = new JTextField(100);
+    private JTextField fingerTextField14 = new JTextField(100);	    
+    private JTextField fingerTextField15 = new JTextField(100);
+
+    private JButton getFinger01Button = new JButton("Get Right Thumb");
+    private JButton getFinger02Button = new JButton("Get Right Index");
+    private JButton getFinger03Button = new JButton("Get Right Middle");
+    private JButton getFinger04Button = new JButton("Get Right Ring");
+    private JButton getFinger05Button = new JButton("Get Right Little");
+    private JButton getFinger06Button = new JButton("Get Left Thumb");
+    private JButton getFinger07Button = new JButton("Get Left Index");
+    private JButton getFinger08Button = new JButton("Get Left Middle");
+    private JButton getFinger09Button = new JButton("Get Left Ring");
+    private JButton getFinger10Button = new JButton("Get Left Little");
+    
+    private JButton generateSlapsButton = new JButton("Create Slap");
+    
+    private File currentDirectory;
 	
     public static void main( String[] args )
     {
@@ -67,6 +86,8 @@ public class App implements ActionListener
     
    public void run() {
 	   
+	    this.currentDirectory = FileSystemView.getFileSystemView().getHomeDirectory();
+	    
 	    JLabel fingerLabel01 = new JLabel("Finger01 - Right Thumb");
 	    JLabel fingerLabel02 = new JLabel("Finger02 - Right Index");
 	    JLabel fingerLabel03 = new JLabel("Finger03 - Right Middle");
@@ -80,7 +101,8 @@ public class App implements ActionListener
 	    JLabel fingerLabel13 = new JLabel("Finger13 - Right Slap");
 	    JLabel fingerLabel14 = new JLabel("Finger14 - Left Slap");
 	    JLabel fingerLabel15 = new JLabel("Finger15 - Thumb Slap");
-	    
+	    JLabel fingerHeading = new JLabel("***** Input Finger Prints *****");
+	    JLabel slapsHeading = new JLabel("***** Output Slaps *****");
 	    
 	    JPanel fingerPanel01 = new JPanel();
 	    JPanel fingerPanel02 = new JPanel();
@@ -95,49 +117,63 @@ public class App implements ActionListener
 	    JPanel fingerPanel13 = new JPanel();
 	    JPanel fingerPanel14 = new JPanel();
 	    JPanel fingerPanel15 = new JPanel();
+	    JPanel fingerHeadingPanel = new JPanel();    
+	    JPanel slapsHeadingPanel = new JPanel();    
 	    
 	    JPanel fingerPanel = new JPanel();
 	    fingerPanel.setLayout(new BoxLayout(fingerPanel, BoxLayout.Y_AXIS));
 	   
+	    
+	    
 	    fingerPanel01.setLayout(new FlowLayout());
 	    fingerPanel01.add(fingerLabel01);
 	    fingerPanel01.add(fingerTextField01);
+	    fingerPanel01.add(getFinger01Button);
 	    
-	    fingerPanel02.setLayout(new FlowLayout());
+	    fingerPanel02.setLayout(new FlowLayout());	    
 	    fingerPanel02.add(fingerLabel02);
 	    fingerPanel02.add(fingerTextField02);
+	    fingerPanel02.add(getFinger02Button);
 	    
 	    fingerPanel03.setLayout(new FlowLayout());
 	    fingerPanel03.add(fingerLabel03);
 	    fingerPanel03.add(fingerTextField03);
+	    fingerPanel03.add(getFinger03Button);
 	    
 	    fingerPanel04.setLayout(new FlowLayout());
 	    fingerPanel04.add(fingerLabel04);
 	    fingerPanel04.add(fingerTextField04);
+	    fingerPanel04.add(getFinger04Button);
 	    
 	    fingerPanel05.setLayout(new FlowLayout());
 	    fingerPanel05.add(fingerLabel05);
 	    fingerPanel05.add(fingerTextField05);
-	    
+	    fingerPanel05.add(getFinger05Button);
+	    	    
 	    fingerPanel06.setLayout(new FlowLayout());
 	    fingerPanel06.add(fingerLabel06);
 	    fingerPanel06.add(fingerTextField06);
+	    fingerPanel06.add(getFinger06Button);
 	    
 	    fingerPanel07.setLayout(new FlowLayout());
 	    fingerPanel07.add(fingerLabel07);
 	    fingerPanel07.add(fingerTextField07);
+	    fingerPanel07.add(getFinger07Button);
 	    
 	    fingerPanel08.setLayout(new FlowLayout());
 	    fingerPanel08.add(fingerLabel08);
 	    fingerPanel08.add(fingerTextField08);
+	    fingerPanel08.add(getFinger08Button);
 	    
 	    fingerPanel09.setLayout(new FlowLayout());
 	    fingerPanel09.add(fingerLabel09);
 	    fingerPanel09.add(fingerTextField09);
+	    fingerPanel09.add(getFinger09Button);
 	    
 	    fingerPanel10.setLayout(new FlowLayout());
 	    fingerPanel10.add(fingerLabel10);
 	    fingerPanel10.add(fingerTextField10);
+	    fingerPanel10.add(getFinger10Button);
 	    
 	    fingerPanel13.setLayout(new FlowLayout());
 	    fingerPanel13.add(fingerLabel13);
@@ -151,13 +187,32 @@ public class App implements ActionListener
 	    fingerPanel15.add(fingerLabel15);
 	    fingerPanel15.add(fingerTextField15);
 	    
+	    
+	    fingerHeadingPanel.setLayout(new FlowLayout());
+	    fingerHeadingPanel.add(fingerHeading);
+	    
+	    slapsHeadingPanel.setLayout(new FlowLayout());
+	    slapsHeadingPanel.add(slapsHeading);
+	    
         JFrame frame = new JFrame("SlapCreator");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(800,600);
 
                 
-        JButton button = new JButton("Create Slap");
-        button.addActionListener(this);
+
+        this.generateSlapsButton.addActionListener(this);
+        this.getFinger01Button.addActionListener(this);
+        this.getFinger02Button.addActionListener(this);
+        this.getFinger03Button.addActionListener(this);
+        this.getFinger04Button.addActionListener(this);
+        this.getFinger05Button.addActionListener(this);
+        this.getFinger06Button.addActionListener(this);
+        this.getFinger07Button.addActionListener(this);
+        this.getFinger08Button.addActionListener(this);
+        this.getFinger09Button.addActionListener(this);
+        this.getFinger10Button.addActionListener(this);       
+        
+        fingerPanel.add(fingerHeadingPanel);        
         fingerPanel.add(fingerPanel01);
         fingerPanel.add(fingerPanel02);
         fingerPanel.add(fingerPanel03);
@@ -168,37 +223,79 @@ public class App implements ActionListener
         fingerPanel.add(fingerPanel08);
         fingerPanel.add(fingerPanel09);
         fingerPanel.add(fingerPanel10);
+
+        fingerPanel.add(slapsHeadingPanel);
         fingerPanel.add(fingerPanel13);
         fingerPanel.add(fingerPanel14);
         fingerPanel.add(fingerPanel15);
         
         
+
         frame.add(fingerPanel,  BorderLayout.CENTER);
     
-        frame.getContentPane().add(button, BorderLayout.PAGE_END);
+        frame.getContentPane().add(generateSlapsButton, BorderLayout.PAGE_END);
         frame.pack();
         frame.setVisible(true);   
     }
     
     
     public void actionPerformed(ActionEvent e) {
-    	   byte[] finger01 = getBytesFromFile(fingerTextField01.getText());
-    	   byte[] finger02 = getBytesFromFile(fingerTextField02.getText());
-    	   byte[] finger03 = getBytesFromFile(fingerTextField03.getText());
-    	   byte[] finger04 = getBytesFromFile(fingerTextField04.getText());
-    	   byte[] finger05 = getBytesFromFile(fingerTextField05.getText());
-    	   byte[] finger06 = getBytesFromFile(fingerTextField06.getText());
-    	   byte[] finger07 = getBytesFromFile(fingerTextField07.getText());
-    	   byte[] finger08 = getBytesFromFile(fingerTextField08.getText());
-    	   byte[] finger09 = getBytesFromFile(fingerTextField09.getText());
-    	   byte[] finger10 = getBytesFromFile(fingerTextField10.getText());
-    	   byte[] slap13 = SlapGen.rightSlap(finger02, finger03, finger04, finger05);
-    	   byte[] slap14 = SlapGen.leftSlap(finger07, finger08, finger09, finger10);
-    	   byte[] slap15 = SlapGen.thumbSlap(finger01, finger06);
-    	   
-    	   writeSlapToFile(fingerTextField13.getText(), slap13);
-    	   writeSlapToFile(fingerTextField14.getText(), slap14);
-    	   writeSlapToFile(fingerTextField15.getText(), slap15);
+    	
+    	if (e.getSource() == this.generateSlapsButton) {
+	    	processSlaps();
+	    	return;
+    	}
+    	
+    	if (e.getSource() == this.getFinger01Button) {
+    		getFileName(this.fingerTextField01);
+    		return;
+    	}    	
+    
+    	if (e.getSource() == this.getFinger02Button) {
+    		getFileName(this.fingerTextField02);
+    		return;
+    	}
+    	
+    	if (e.getSource() == this.getFinger03Button) {
+    		getFileName(this.fingerTextField03);
+    		return;
+    	}
+    	
+    	if (e.getSource() == this.getFinger04Button) {
+    		getFileName(this.fingerTextField04);
+    		return;
+    	}
+    	
+    	if (e.getSource() == this.getFinger05Button) {
+    		getFileName(this.fingerTextField05);
+    		return;
+    	}
+    	
+    	if (e.getSource() == this.getFinger06Button) {
+    		getFileName(this.fingerTextField06);
+    		return;
+    	}
+    	
+    	if (e.getSource() == this.getFinger07Button) {
+    		getFileName(this.fingerTextField07);
+    		return;
+    	}
+    	
+    	if (e.getSource() == this.getFinger08Button) {
+    		getFileName(this.fingerTextField08);
+    		return;
+    	}
+    	
+    	if (e.getSource() == this.getFinger09Button) {
+    		getFileName(this.fingerTextField09);
+    		return;
+    	}
+    	
+    	if (e.getSource() == this.getFinger10Button) {
+    		getFileName(this.fingerTextField10);
+    		return;
+    	}
+  	
     }
     
     private byte[] getBytesFromFile(String fileName){
@@ -232,7 +329,7 @@ public class App implements ActionListener
     
     private void writeSlapToFile(String fileName, byte[] slap) {
     	if(fileName.trim().compareTo("")==0) {
-    		System.out.println("Unable to wirte slap - no file name!!");
+    		System.out.println("Unable to write slap - no file name!!");
     	}
     	
     	if(fileName.trim().length() > 4 && 
@@ -258,6 +355,45 @@ public class App implements ActionListener
 	    	}
 	    	System.out.println( "**** Unknown filetype - Please use TXT/WSQ : " + fileName );
 	    	}
+    
+    private void processSlaps() {
+ 	   byte[] finger01 = getBytesFromFile(fingerTextField01.getText());
+ 	   byte[] finger02 = getBytesFromFile(fingerTextField02.getText());
+ 	   byte[] finger03 = getBytesFromFile(fingerTextField03.getText());
+ 	   byte[] finger04 = getBytesFromFile(fingerTextField04.getText());
+ 	   byte[] finger05 = getBytesFromFile(fingerTextField05.getText());
+ 	   byte[] finger06 = getBytesFromFile(fingerTextField06.getText());
+ 	   byte[] finger07 = getBytesFromFile(fingerTextField07.getText());
+ 	   byte[] finger08 = getBytesFromFile(fingerTextField08.getText());
+ 	   byte[] finger09 = getBytesFromFile(fingerTextField09.getText());
+ 	   byte[] finger10 = getBytesFromFile(fingerTextField10.getText());
+ 	   byte[] slap13 = SlapGen.rightSlap(finger02, finger03, finger04, finger05);
+ 	   byte[] slap14 = SlapGen.leftSlap(finger07, finger08, finger09, finger10);
+ 	   byte[] slap15 = SlapGen.thumbSlap(finger01, finger06);
+ 	   
+ 	   writeSlapToFile(fingerTextField13.getText(), slap13);
+ 	   writeSlapToFile(fingerTextField14.getText(), slap14);
+ 	   writeSlapToFile(fingerTextField15.getText(), slap15);
+    }
+    
+   private void getFileName(JTextField fingerTextField) {
+	   
+	    JFileChooser jfc;
+	    if(fingerTextField.getText().compareTo("")==0) {
+	    	jfc = new JFileChooser(this.currentDirectory);
+	    }else {
+	    	jfc = new JFileChooser(new File(fingerTextField.getText()));
+	    }
+	    jfc.setDialogTitle("Select FingerPrint");
+	    FileNameExtensionFilter filter = new FileNameExtensionFilter("Fingerprint Files", "wsq", "txt");
+	    jfc.addChoosableFileFilter(filter);
+	    int returnValue = jfc.showOpenDialog(null);
+		if (returnValue == JFileChooser.APPROVE_OPTION) {
+			fingerTextField.setText(jfc.getSelectedFile().getPath());
+			this.currentDirectory = jfc.getSelectedFile();
+		}
+	    
+   }
    }   
 
 
